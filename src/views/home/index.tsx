f796f2a665cd0eae43b05aff0fb20cef887a405c
nnecec <nnecec@outlook.com>
2018-04-13 20:48:15 +0800

    update

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -21,7 +21,7 @@ class Home extends React.Component {
             <span className="font-hei title">nnecec</span>
           </div>
         </section>
-        <section className="home-section me" style={{ height, marginTop: height }}>
+        <section className="home-section me" style={{ height }}>
           <Parallax className="parallax-container" ref="parallax" pages={3} horizontal scrolling={false}>
             <Page offset={0} gradient="pink" caption="who we are" first="Lorem ipsum" second="dolor sit" onClick={() => this.scroll(1)} />
             <Page offset={1} gradient="teal" caption="what we do" first="consectetur" second="adipiscing elit" onClick={() => this.scroll(2)} />
